10b6e7d2d7a9de59400aebbcdbd1688dcd1674ce
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-22 21:28:38 +0100

    ADD explanations to the code

--- main.py
+++ main.py
@@ -2,22 +2,28 @@ from tools.database_tool import Db_tool
 from tools.json_tool import Json_tool
 import argparse
 
+"""
+    Run the given code below in the CLI to run the project
+
+    python3 main.py --file "dataregex.json" --db "dataregex.db"
+"""
+
 def main():
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--file", type=str, required=True)
-    parser.add_argument("--db", type=str, required=True)
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser() #Create the parser
+    parser.add_argument("--file", type=str, required=True) #Add --file argument
+    parser.add_argument("--db", type=str, required=True) #Add --db argument
+    args = parser.parse_args() #Parse the argument
 
-    file = args.file
-    db = args.db
+    file = args.file #Set file as argument
+    db = args.db #Set db as argument
 
-    file_process = Json_tool(file) #"dataregex.json"
-    db_process = Db_tool(db) #"dataregex.db"
-    db_process.create_table()
-    access_user_data = file_process.create_user_from_data()
+    file_process = Json_tool(file) #Calls -> "dataregex.json"
+    db_process = Db_tool(db) #Calls -> "dataregex.db"
+    db_process.create_table() #Create table
+    access_user_data = file_process.create_user_from_data() #Access created users
 
-    for each_user in access_user_data:
-        db_process.insert_data(each_user)
+    for each_user in access_user_data: #Iterate over accessed user data
+        db_process.insert_data(each_user) #Insert those to the database
 
 main()
