fff0c7ee7f9dc90bcd2395044e869dd73ffef56e
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-22 19:40:51 +0100

    ADD json_tool.py

---
+++ tools/json_tool.py
@@ -0,0 +1,10 @@
+class Json_tool():
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def get_json(self):
+        pass
+
+    def create_user_from_data(self):
+        pass
