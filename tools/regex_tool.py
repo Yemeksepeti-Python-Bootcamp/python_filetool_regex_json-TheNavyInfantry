0eaa04970b1ce3f158bfec43e340b172a2ed67df
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-22 21:28:53 +0100

    ADD explanations to the code

--- tools/regex_tool.py
+++ tools/regex_tool.py
@@ -3,6 +3,7 @@ import re
 class Regex_tool():
 
     def birth_date_validate(self, birth_date):
+        """Validate birth date, split into year(doy), month(dom), day(dod) and return respectively"""
         pattern = r'([0-9]{4})-([0-9]{2})-([0-9]{2})'
         match = re.search(pattern, birth_date)
         doy, dom, dod = match.group(1), match.group(2), match.group(3)
@@ -10,6 +11,8 @@ class Regex_tool():
         return doy, dom, dod
 
     def email_validate(self, email, username):
+        """Validate both email and if email contains at least three consecutive letters.
+            Return 1 if True, return 0 if False"""
         pattern_username = r'(^.{0,3})'
         match_username = re.search(pattern_username, username)
         username_group = match_username.group(1)
@@ -21,6 +24,7 @@ class Regex_tool():
         return 1 if username_group in email_group else 0
 
     def username_validate(self, username, name_surname):
+        """Validate if the username contains the name"""
         pattern = r"^\b(\w+)\b(.*)\b(\w+)\b"
         match = re.search(pattern, name_surname)
         name_group = match.group(1)
