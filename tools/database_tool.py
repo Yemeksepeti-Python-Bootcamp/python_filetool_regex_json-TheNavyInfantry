01a11d9ec5b5c8e5d25730cfe33ca8a452757441
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-22 19:40:26 +0100

    ADD database_tool.py

---
+++ tools/database_tool.py
@@ -0,0 +1,15 @@
+class Db_tool(object):
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    def create_table_name(self):
+        pass
+
+    def create_connection(self):
+        pass
+
+    def create_table(self):
+        pass
+
+    def insert_data(self):
+        pass
