5f46c40ff2f661a080d9c1378fbcb2978e61b883
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-22 19:53:03 +0100

    UPDATE operations on main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from tools.database_tool import Db_tool
+from tools.json_tool import Json_tool
+
 def main():
-    pass
+
+    file_process = Json_tool("dataregex.json")
+    db_process = Db_tool("dataregex.db")
+    db_process.create_table()
+    access_user_data = file_process.create_user_from_data()
+
+    for each_user in access_user_data:
+        db_process.insert_data(each_user)
 
 main()
