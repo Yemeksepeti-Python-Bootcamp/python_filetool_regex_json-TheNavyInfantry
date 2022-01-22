e271f906bd4863c2dfe79885f4125a4a68543c4d
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-22 21:28:46 +0100

    ADD explanations to the code

--- tools/database_tool.py
+++ tools/database_tool.py
@@ -6,28 +6,32 @@ from datetime import datetime
 
 class Db_tool(object):
     def __init__(self, db_name):
+        """Initialize database name"""
         self.db_name = db_name
 
     def create_table_name(self):
+        """Creates a table name with timestamp"""
         timestamp = datetime.strftime(datetime.now(), str("%Y%m%d_%H%M%S"))
         table_name = "data_" + timestamp
 
         return table_name
 
     def create_connection(self):
-        """ create a database connection to a SQLite database """
+        """Creates a database connection to a SQLite database"""
         try:
-            conn = sqlite3.connect(self.db_name)
-            return conn
+            conn = sqlite3.connect(self.db_name) #Try to connect to the requested database
+            return conn #Return connection if successfull
 
-        except Error as e:
+        except Error as e: #Return error message if any error occurs during connection
             return e
 
     def create_table(self):
-        conn = self.create_connection()
+        """Creates a table for the database"""
+        conn = self.create_connection() #Try to connect to the requested database
         try:
-            cur = conn.cursor()
+            cur = conn.cursor() #Connect cursor with connection which is our operator for operations
 
+            """Table query"""
             table_query = f"""CREATE TABLE {self.create_table_name()} (
                                 email text,
                                 username text,
@@ -40,29 +44,31 @@ class Db_tool(object):
                                 state text,
                                 a_p integer default 1);"""
 
-            cur.execute(table_query)
+            cur.execute(table_query) #Execute the given table query
 
-        except Error as e:
+        except Error as e: #Return error message if any error occurs during connection
             return e
 
         finally:
-            conn.close()
+            conn.close() #Close connection to database
 
     def insert_data(self, user: User):
-        conn = self.create_connection()
+        """Inserts data to the table"""
+        conn = self.create_connection() #Try to connect to the requested database
         try:
+            """Insert query"""
             insert_query = f'''
             INSERT INTO {self.create_table_name()}
             (email,username,name_surname,emailuserlk,usernamelk, doy, dom, dod, state, a_p)
             VALUES (?,?,?,?,?,?,?,?,?,?)'''
 
-            cur = conn.cursor()
+            cur = conn.cursor() #Connect cursor with connection which is our operator for operations
             cur.execute(insert_query, (user.email, user.username, user.name_surname, user.emailuserlk, user.usernamelk,
-                                       user.doy, user.dom, user.dod, user.state, user.a_p))
-            conn.commit()
+                                       user.doy, user.dom, user.dod, user.state, user.a_p)) #Execute the given insert query
+            conn.commit() #Commit ends a transaction within database
 
-        except Error as e:
+        except Error as e: #Return error message if any error occurs during connection
             return e
 
         finally:
-            conn.close()
+            conn.close() #Close connection to database
