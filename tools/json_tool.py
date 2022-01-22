44f710b119bc4a732bf47ff46c5c299ba463693e
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-22 19:46:22 +0100

    UPDATE all methods on json_tool.py

--- tools/json_tool.py
+++ tools/json_tool.py
@@ -1,10 +1,35 @@
+from models.db_model import User
+from tools.regex_tool import Regex_tool
+import json
+
+
 class Json_tool():
 
     def __init__(self, filename):
         self.filename = filename
 
     def get_json(self):
-        pass
+        with open(self.filename, "r") as json_file:
+            data = json.load(json_file)
+
+            return data
 
     def create_user_from_data(self):
-        pass
+        json_data = self.get_json()
+        rgx = Regex_tool()
+
+        user_data = list()
+
+        for data in json_data:
+            user = User()
+            user.email = data["email"]
+            user.username = data["username"]
+            user.name_surname = data["profile"]["name"]
+            user.emailuserlk = rgx.email_validate(data["email"], data["username"])
+            user.usernamelk = rgx.username_validate(data["username"], data["profile"]["name"])
+            user.doy, user.dom, user.dod = rgx.birth_date_validate(data["profile"]["dob"])
+            user.state = data.get("profile").get("address").split()[-1]
+
+            user_data.append(user)
+
+        return user_data
